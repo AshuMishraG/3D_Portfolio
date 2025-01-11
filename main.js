7d927a229f3b54226e12e3048c13a6e1db36e1d0
AshuMishraG <aashutoshm77@gmail.com>
2025-01-12 05:07:13 +0530

    starting the project
    Rotating the 3D cube

--- main.js
+++ main.js
@@ -4,17 +4,17 @@ const scene = new THREE.Scene();
 scene.background = new THREE.Color("#F0F0F0");
 
 const camera = new THREE.PerspectiveCamera(
-   75,
-   window.innerWidth / window.innerHeight,
-   0.1,
-   1000
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000,
 );
 camera.position.z = 5;
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshLambertMaterial({
-   color: "#468585",
-   emissive: "#468585",
+  color: "#468585",
+  emissive: "#468585",
 });
 
 const cube = new THREE.Mesh(geometry, material);
@@ -28,5 +28,13 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-renderer.render(scene, camera);
+function animate() {
+  requestAnimationFrame(animate);
 
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+animate();
