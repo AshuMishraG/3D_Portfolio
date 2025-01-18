98e4cef7c492d4328edeaa6b1b5201b4ee8a77f1
AshuMishraG <aashutoshm77@gmail.com>
2025-01-18 09:31:46 +0530

    OrbitControls
    6 and 7th layer added

--- vite/src/main.js
+++ vite/src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const canvas = document.getElementById("canvas");
 
@@ -35,7 +36,27 @@ scene.add(light);
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
-
 renderer.setSize(window.innerWidth, window.innerHeight);
 
-renderer.render(scene, camera);
+// OrbitControls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
+controls.enableZoom = true;
+controls.enablePan = true;
+
+// Animations
+function animate() {
+  requestAnimationFrame(animate);
+
+  dodecahedron.rotation.x += 0.01;
+  dodecahedron.rotation.y += 0.01;
+
+  box.rotation.y += 0.005;
+
+  controls.update();
+
+  renderer.render(scene, camera);
+}
+
+animate();
