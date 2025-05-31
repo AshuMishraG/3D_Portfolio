8353353d7fc515ae8f0c96974f575b37b4cf84de
AshuMishraG <aashutoshm77@gmail.com>
2025-05-31 12:18:25 +0530

    feat: make company icons clickable and apply shining gradient to heading

--- threejs_portfolio/src/sections/Experience.jsx
+++ threejs_portfolio/src/sections/Experience.jsx
@@ -7,70 +7,93 @@ import CanvasLoader from "../components/Loading.jsx";
 import { workExperiences } from "../constants/index.js";
 
 const WorkExperience = () => {
-  const [animationName, setAnimationName] = useState("idle");
+   const [animationName, setAnimationName] = useState("idle");
 
-  return (
-    <section className="c-space my-20" id="work">
-      <div className="w-full text-white-600">
-        <p className="head-text">My Work Experience</p>
+   return (
+      <section className="c-space my-20" id="work">
+         <div className="w-full text-white-600">
+            <p className="head-text text-center shining-gradient">
+               My Work Experience
+            </p>
 
-        <div className="work-container">
-          <div className="work-canvas">
-            <Canvas>
-              <ambientLight intensity={7} />
-              <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-              <directionalLight position={[10, 10, 10]} intensity={1} />
-              <OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} />
+            <div className="work-container">
+               <div className="work-canvas">
+                  <Canvas>
+                     <ambientLight intensity={7} />
+                     <spotLight
+                        position={[10, 10, 10]}
+                        angle={0.15}
+                        penumbra={1}
+                     />
+                     <directionalLight position={[10, 10, 10]} intensity={1} />
+                     <OrbitControls
+                        enableZoom={false}
+                        maxPolarAngle={Math.PI / 2}
+                     />
 
-              <Suspense fallback={<CanvasLoader />}>
-                <Developer
-                  position-y={-3}
-                  scale={3}
-                  animationName={animationName}
-                />
-              </Suspense>
-            </Canvas>
-          </div>
+                     <Suspense fallback={<CanvasLoader />}>
+                        <Developer
+                           position-y={-3}
+                           scale={3}
+                           animationName={animationName}
+                        />
+                     </Suspense>
+                  </Canvas>
+               </div>
 
-          <div className="work-content">
-            <div className="sm:py-10 py-5 sm:px-5 px-2.5">
-              {workExperiences.map((item, index) => (
-                <div
-                  key={index}
-                  onClick={() => setAnimationName(item.animation.toLowerCase())}
-                  onPointerOver={() =>
-                    setAnimationName(item.animation.toLowerCase())
-                  }
-                  onPointerOut={() => setAnimationName("idle")}
-                  className="work-content_container group"
-                >
-                  <div className="flex flex-col h-full justify-start items-center py-2">
-                    <div className="work-content_logo">
-                      <img className="w-full h-full" src={item.icon} alt="" />
-                    </div>
+               <div className="work-content">
+                  <div className="sm:py-10 py-5 sm:px-5 px-2.5">
+                     {workExperiences.map((item, index) => (
+                        <div
+                           key={index}
+                           onClick={() =>
+                              setAnimationName(item.animation.toLowerCase())
+                           }
+                           onPointerOver={() =>
+                              setAnimationName(item.animation.toLowerCase())
+                           }
+                           onPointerOut={() => setAnimationName("idle")}
+                           className="work-content_container group"
+                        >
+                           <div className="flex flex-col h-full justify-start items-center py-2">
+                              <div className="work-content_logo">
+                                 <a
+                                    href={item.link}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                 >
+                                    <img
+                                       className="w-full h-full"
+                                       src={item.icon}
+                                       alt={item.name}
+                                    />
+                                 </a>
+                              </div>
 
-                    <div className="work-content_bar" />
+                              <div className="work-content_bar" />
+                           </div>
+
+                           <div className="sm:p-5 px-2.5 py-5">
+                              <p className="font-bold text-white-800">
+                                 {item.name}
+                              </p>
+                              <p className="text-sm mb-5">
+                                 {item.pos} -- <span>{item.duration}</span>
+                              </p>
+                              <ul className="list-disc pl-5 group-hover:text-white transition-all ease-in-out duration-500">
+                                 {item.points.map((point, i) => (
+                                    <li key={i}>{point}</li>
+                                 ))}
+                              </ul>
+                           </div>
+                        </div>
+                     ))}
                   </div>
-
-                  <div className="sm:p-5 px-2.5 py-5">
-                    <p className="font-bold text-white-800">{item.name}</p>
-                    <p className="text-sm mb-5">
-                      {item.pos} -- <span>{item.duration}</span>
-                    </p>
-                    <ul className="list-disc pl-5 group-hover:text-white transition-all ease-in-out duration-500">
-                      {item.points.map((point, i) => (
-                        <li key={i}>{point}</li>
-                      ))}
-                    </ul>
-                  </div>
-                </div>
-              ))}
+               </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+         </div>
+      </section>
+   );
 };
 
 export default WorkExperience;
